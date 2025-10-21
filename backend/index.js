67ff0a5f2e28c7cc112351faaab06eb5cbd9f7b1
Prince Hlela <mphdigital@protonmail.com>
2025-10-21 21:17:41 +0200

    Add express-validator and Vercel config
    Added express-validator to dependencies and implemented validators for products and contact forms. Introduced vercel.json for deployment configuration. Also improved code formatting in index.js and validators.js for consistency.

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const cors =require('cors');
+const cors = require('cors');
 
-const {dbConnection}=require('./config/db');
+const { dbConnection } = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
-const contactRoute=require('./routes/contactRoutes');
+const contactRoute = require('./routes/contactRoutes');
 // Connect to Database
 dbConnection();
 //express app initialization
 const app = express();
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit:"300mb"}));
-app.use(express.urlencoded({ extended: true,limit:"300mb" }));
+app.use(express.json({ limit: "300mb" }));
+app.use(express.urlencoded({ extended: true, limit: "300mb" }));
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN || '*'
+    origin: process.env.CORS_ORIGIN || '*'
 }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.use('/api/products', productRoutes);
-app.use('/api/contact',contactRoute)
+app.use('/api/contact', contactRoute)
 
 app.listen(PORT, () => {
     console.log(`app running on http://localhost: ${PORT}`);
--- backend/utils/validators.js
+++ backend/utils/validators.js
@@ -1,15 +1,15 @@
-const {body}=require('express-validator');
+const { body } = require('express-validator');
 
 // Example validator for product creation
-const validateProducts=[
-body('title').notEmpty().withMessage('Title of the product is require'),
-body('weight').notEmpty().withMessage('Weight of the product is required'),
-body('images').isArray().notEmpty().withMessage('Images must be an array of image files'),
+const validateProducts = [
+  body('title').notEmpty().withMessage('Title of the product is require'),
+  body('weight').notEmpty().withMessage('Weight of the product is required'),
+  body('images').isArray().notEmpty().withMessage('Images must be an array of image files'),
 ];
 
-const validateContact=[
+const validateContact = [
   body('email').isEmail().withMessage('Valid email is required'),
   body('message').notEmpty().withMessage('Message is required'),
 ];
 
-module.exports={validateProducts,validateContact};
+module.exports = { validateProducts, validateContact };

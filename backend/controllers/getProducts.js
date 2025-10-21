f145a46c21ba96fb16e8f37d1197197a9e5b28d7
Thandeka Radebe <thandekaradebe81@gmail.com>
2025-10-21 14:51:41 +0200

    Added getProducts.js file from main branch

---
+++ backend/controllers/getProducts.js
@@ -0,0 +1,20 @@
+// ==================== CONTROLLERS/PRODUCTCONTROLLER.JS
+const Product = require("../models/products");
+
+// Get all products
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        message: "Product successsfully retrieved",
+        data: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+        success: false,
+        message: "Fetching product unsuccessful",
+        error: error.message,
+    });
+  }
+};

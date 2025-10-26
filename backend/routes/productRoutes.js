43482be0508c3bc1e833d94081f06857c5b9afa7
Prince Hlela <mphdigital@protonmail.com>
2025-10-26 15:05:47 +0200

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, searchProducts, getProducts } = require('../controllers/productController');
+const { createProduct, searchProducts } = require('../controllers/productController');
+const { getProducts } = require('../controllers/getProducts');
 const { authenticate } = require('../middleware/authMiddleware')
 
 router.post('/', authenticate, createProduct);
-// router.get('/', getProducts);
+router.get('/', getProducts);
 router.get('/search', searchProducts);
 module.exports = router;
